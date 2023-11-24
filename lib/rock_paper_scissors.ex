880f8ef904350556a36c94368482c28744f656f1
Brianna St Clair <briannastclair13@gmail.com>
2023-11-24 12:09:37 -0500

    finished wordle game

--- lib/rock_paper_scissors.ex
+++ lib/rock_paper_scissors.ex
@@ -2,16 +2,16 @@ defmodule Games.RockPaperScissors do
   def play do
     choices = ["rock", "paper", "scissors"]
     ai_answer = Enum.random(choices)
-  users_answer = IO.gets("Choose rock, paper, or scissors- ") |> String.trim_trailing()
+    users_answer = IO.gets("Choose rock, paper, or scissors- ") |> String.trim_trailing()
 
-  case {ai_answer, users_answer} do
-    {"rock", "scissors"} -> IO.puts("ai chose rock. You lose! Rock beats scissors")
-    {"paper", "rock"} -> IO.puts("ai chose paper. You lose! Paper beats rock")
-    {"scissors", "paper"} -> IO.puts("ai chose scissors. You lose! Scissors beats paper")
-    {"rock", "paper"} -> IO.puts("ai chose rock. You win! Paper beats rock")
-    {"paper", "scissors"} -> IO.puts("ai chose paper. You win! Scissors beats paper")
-    {"scissors", "rock"} -> IO.puts("ai chose scissors. You win! Rock beats scissors")
-    {_same, _same} -> IO.puts("ai chose the same thing as you. Draw")
-end
-end
+    case {ai_answer, users_answer} do
+      {"rock", "scissors"} -> IO.puts("ai chose rock. You lose! Rock beats scissors")
+      {"paper", "rock"} -> IO.puts("ai chose paper. You lose! Paper beats rock")
+      {"scissors", "paper"} -> IO.puts("ai chose scissors. You lose! Scissors beats paper")
+      {"rock", "paper"} -> IO.puts("ai chose rock. You win! Paper beats rock")
+      {"paper", "scissors"} -> IO.puts("ai chose paper. You win! Scissors beats paper")
+      {"scissors", "rock"} -> IO.puts("ai chose scissors. You win! Rock beats scissors")
+      {_same, _same} -> IO.puts("ai chose the same thing as you. Draw")
+    end
+  end
 end
